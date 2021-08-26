19c533382828be604f9bcdd1b98440e87785ed56
Bia863086 <beatrizcampos2101@gmail.com>
2021-08-26 16:17:43 -0300

    eu coloquei o nome de uma escola

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace etec_adolpho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
